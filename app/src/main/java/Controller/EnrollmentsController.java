61b6890831726e0f0d7fedb4576ee5ab74460802
EmadKh98 <imad.khadra@technolead.com>
2020-12-23 15:34:46 +0200

    View My Subject

--- app/src/main/java/Controller/EnrollmentsController.java
+++ app/src/main/java/Controller/EnrollmentsController.java
@@ -7,12 +7,38 @@ import android.database.sqlite.SQLiteDatabase;
 import java.util.ArrayList;
 import java.util.List;
 
+import Models.Course;
 import Models.Enrollment;
 import Models.ViewModels.StudentsEnrollmentInfoVM;
 
 
 public class EnrollmentsController {
 
+    public static List<StudentsEnrollmentInfoVM> GetStudentEnrollments(DBContext context, int StudentId)
+    {
+        SQLiteDatabase database = context.getReadableDatabase() ;
+        List<StudentsEnrollmentInfoVM> studentsEnrollmentsList = new ArrayList<>();
+        String getAll = "Select Students.Id ,Students.FirstName || ' ' || Students.LastName ,Sections.SectionName" +
+                " ,Courses.Title,Instructors.FirstName || ' ' || Instructors.LastName" +
+                " From Students,Enrollments,Instructors,Sections ,Courses,CoursesinSections " +
+                " where CoursesinSections.CourseId = Courses.CourseId And CoursesinSections.SectionNo = Sections.SectionNo" +
+                " And CoursesinSections.InstructorId = Instructors.InstructorId " +
+                " And Enrollments.StudentId = Students.Id And Enrollments.CourseId = Courses.CourseId " +
+                " And Enrollments.SectionNo = Sections.SectionNo And Students.Id = " + StudentId + " ; ";
+
+        Cursor cursor = database.rawQuery(getAll ,null );
+        if (cursor.moveToFirst())
+            do{
+                StudentsEnrollmentInfoVM studentsEnrollmentInfo = new StudentsEnrollmentInfoVM();
+                studentsEnrollmentInfo.setStudentId(cursor.getInt(0));
+                studentsEnrollmentInfo.setStudentName(cursor.getString(1));
+                studentsEnrollmentInfo.setSectionName(cursor.getString(2));
+                studentsEnrollmentInfo.setCourseTitle(cursor.getString(3));
+                studentsEnrollmentInfo.setInstructorName(cursor.getString(4));
+                studentsEnrollmentsList.add(studentsEnrollmentInfo);
+            }while (cursor.moveToNext());
+        return  studentsEnrollmentsList;
+    }
     public static List<Enrollment> getEnrollments(DBContext context) {
         SQLiteDatabase database = context.getReadableDatabase() ;
         List<Enrollment> EnrollmentsList = new ArrayList<>();
--- app/src/main/java/com/example/iwb303/tab/MySubjects/MySubjectsFragment.java
+++ app/src/main/java/com/example/iwb303/tab/MySubjects/MySubjectsFragment.java
@@ -19,6 +19,14 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.iwb303.R;
 
+import java.util.List;
+
+import Controller.DBContext;
+import Controller.EnrollmentsController;
+import Controller.StudentsController;
+import Models.ViewModels.StudentInfoVM;
+import Models.ViewModels.StudentsEnrollmentInfoVM;
+
 public class MySubjectsFragment extends Fragment {
 
     TextView lblUname;
@@ -27,7 +35,18 @@ public class MySubjectsFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_my_subjects, container, false);
         lblUname = root.findViewById(R.id.lblSubjects);
         SharedPreferences settings = getActivity().getSharedPreferences("UserInfo", 0);
-        lblUname.setText(settings.getString("Username", "").toString());
+        lblUname.setText(settings.getString("Username", ""));
+        Integer studentId = settings.getInt("Id",0);
+        StringBuffer buffer=new StringBuffer();
+        DBContext context = new DBContext(getActivity());
+        List<StudentsEnrollmentInfoVM> studentsEnrollmentInfoVM = EnrollmentsController.GetStudentEnrollments(context,studentId);
+        for (StudentsEnrollmentInfoVM u: studentsEnrollmentInfoVM)
+        {
+            buffer.append("Id: "+ u.getSectionName() +"\n");
+            buffer.append("UserName: "+ u.getCourseTitle() +"\n");
+            buffer.append("RegYear: "+ u.getInstructorName() +"\n");
+            buffer.append("---------------------------\n");
+        }
         return root;
     }
 
