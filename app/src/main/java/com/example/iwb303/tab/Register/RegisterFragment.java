541aa851207a59c6f637688030116874ca752f36
EmadKh98 <imad.khadra@technolead.com>
2020-12-23 19:02:20 +0200

    Spiner

--- app/src/main/java/com/example/iwb303/tab/Register/RegisterFragment.java
+++ app/src/main/java/com/example/iwb303/tab/Register/RegisterFragment.java
@@ -7,7 +7,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,19 +23,42 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.iwb303.MainActivity;
 import com.example.iwb303.R;
 
+import Controller.DBContext;
+import Controller.SectionsController;
+import Controller.StudentsController;
+import Models.LoginInfo;
+import Models.Section;
+import Models.ViewModels.StudentInfoVM;
+
 public class RegisterFragment extends Fragment implements View.OnClickListener {
 
     Button btnRegister;
     TextView lblSubjects;
     MediaPlayer md;
+    private Spinner spinner;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_register, container, false);
         btnRegister = root.findViewById(R.id.btnRegister);
         btnRegister.setOnClickListener(this);
+        spinner = root.findViewById(R.id.spinner);
+        ArrayAdapter<Section> adapter = new ArrayAdapter<Section>(getActivity(),
+                android.R.layout.simple_spinner_item, SectionsController.GetSections(new DBContext(getActivity())));
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Section section = (Section) parent.getSelectedItem();
+                Toast.makeText(getActivity() ,section.getSectionNo() + " - " + section.getSectionName() , Toast.LENGTH_LONG).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
         return root;
     }
-
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
